9300b9e1801f2ba3e05d3f21e7634b9171c363d1
Arthur Richert--Villain <saulevant@proton.me>
2025-06-25 10:41:54 +0200

    page accueil

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -5,14 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\FormationRepository;
+
 
 final class FormationController extends AbstractController
 {
     #[Route('/formation', name: 'app_formation')]
-    public function index(): Response
+    public function index(FormationRepository $formationRepository): Response
     {
+        $formations = $formationRepository->findAll(); 
+
         return $this->render('formation/index.html.twig', [
-            'controller_name' => 'FormationController',
+            'formations' => $formations,
         ]);
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\FormationRepository;
+
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(FormationRepository $formationRepository): Response
     {
+        $formations = $formationRepository->findAll(); // ou un findBy(['estVisible' => true])
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'formations' => $formations,
         ]);
     }
 }
