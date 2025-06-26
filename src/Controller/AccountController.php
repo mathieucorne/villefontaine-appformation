4f6557f6ef271a1f61a9c72caf977ed5e9fc000d
Mathieu Corne <math.corne@gmail.com>
2025-06-26 14:15:43 +0200

    fix(AccountController) - Correction de l'erreur liée à non-vérification du typage de la variable Utilisateur

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,15 +2,13 @@
 
 namespace App\Controller;
 
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\EntityManagerInterface as ORMEntityManagerInterface;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
-use Symfony\Component\Routing\Attribute\Route;
-use Symfony\ORM\EntityManagerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Utilisateur;
 use App\Repository\ParametreRepository;
 
 
@@ -20,36 +18,38 @@ final class AccountController extends AbstractController
     public function index(
         Request $request,
         UserPasswordHasherInterface $passwordHasher,
-        ORMEntityManagerInterface $entityManager,
+        EntityManagerInterface $entityManager,
         ParametreRepository $parametreRepository
     ): Response
     {
         $user = $this -> getUser();
 
-        if ($request -> isMethod('POST')){
-            $oldPassword = $request->request->get('_old_password');
-            $newPassword = $request->request->get('_new_password');
-            $confirmPassword = $request->request->get('_confirm_password');
+        if ($user instanceof Utilisateur) {
+            if ($request -> isMethod('POST')){
+                $oldPassword = $request->request->get('_old_password');
+                $newPassword = $request->request->get('_new_password');
+                $confirmPassword = $request->request->get('_confirm_password');
 
-            if (!$passwordHasher->isPasswordValid($user, $oldPassword)) {
-                $this->addFlash('error', 'L\'ancien mot de passe est incorrect.');
-            }
+                if (!$passwordHasher->isPasswordValid($user, $oldPassword)) {
+                    $this->addFlash('error', 'L\'ancien mot de passe est incorrect.');
+                }
 
-            elseif ($newPassword == $oldPassword){
-                $this->addFlash('error', 'L\'ancien et le nouveau mot de passe sont similaires');
-            }
+                elseif ($newPassword == $oldPassword){
+                    $this->addFlash('error', 'L\'ancien et le nouveau mot de passe sont similaires');
+                }
 
-            elseif ($newPassword != $confirmPassword){
-                $this->addFlash('error', 'Les nouveaux mots de passes ne correspondent pas');
-            }
+                elseif ($newPassword != $confirmPassword){
+                    $this->addFlash('error', 'Les nouveaux mots de passes ne correspondent pas');
+                }
 
-            else {
-                $hashedPassword = $passwordHasher->hashPassword($user, $newPassword);
-                $user->setPassword($hashedPassword);
-                $entityManager->flush();
+                else {
+                    $hashedPassword = $passwordHasher->hashPassword($user, $newPassword);
+                    $user->setPassword($hashedPassword);
+                    $entityManager->flush();
 
-                $this->addFlash('success', 'Mot de passe mis à jour avec succès.');
-                return $this->redirectToRoute('app_account');
+                    $this->addFlash('success', 'Mot de passe mis à jour avec succès.');
+                    return $this->redirectToRoute('app_account');
+                }
             }
         }
 
