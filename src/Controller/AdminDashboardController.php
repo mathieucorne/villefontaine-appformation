52044a698beffaf171819e26d4cccaba21d93036
Arthur Richert--Villain <saulevant@proton.me>
2025-06-30 10:13:29 +0200

    ajout boutos hexadécimal de admin

--- src/Controller/AdminDashboardController.php
+++ src/Controller/AdminDashboardController.php
@@ -18,8 +18,23 @@ final class AdminDashboardController extends AbstractController
             ->findOneBy(['nom' => 'background_color'])
             ?->getValeur() ?? '#ffffff';
 
+        $utilityButton= $parametreRepository
+            ->findOneBy(['nom' => 'utility_button'])
+            ?->getValeur()?? '#4287f5';
+
+        $warningButton = $parametreRepository
+            ->findOneBy(['nom' => 'warning_button'])
+            ?->getValeur()?? '#fa0202';
+
+        $loginBackgroundColor = $parametreRepository
+            ->findOneBy(['nom' => 'login_background_color'])
+            ?->getValeur() ?? '#A2BF3B';
+
         return $this->render('admin_dashboard/admin_dashboard.html.twig', [
             'background_color' => $backgroundColor,
+            'utility_button' => $utilityButton,
+            'warning_button' => $warningButton, 
+            'login_background_color' => $loginBackgroundColor,
         ]);
     }
 }
