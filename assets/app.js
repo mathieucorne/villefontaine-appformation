571451f323771037c5ce255d60d734721f3fbcc5
Mathieu Corne <math.corne@gmail.com>
2025-06-30 15:03:28 +0200

    feat(formation) - Sélecteur de fichier personnalisé

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,23 @@
 import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
 import './styles/app.css';
+
+const fileSelector = document.querySelector('.file-selector-container')
+const fileSelectorInput = document.querySelector('.file-selector-input')
+
+fileSelector.onclick = () => fileSelectorInput.click();
+fileSelectorInput.onchange = () => {
+    [...fileSelectorInput.files].forEach((file) => {
+        if(typeValidation(file.type)) {
+            console.log(file)
+        }
+    })
+}
+
+function typeValidation(type) {
+    var splitType = type.split('/')[0]
+    if (splitType == "image") {
+        return true
+    }
+}
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
