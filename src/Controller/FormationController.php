50632cef476eefb354174cd481339da640dfa7d4
Arthur Richert--Villain <saulevant@proton.me>
2025-07-01 15:53:12 +0200

    affichage correct des formations sur la page formation

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\FormationRepository;
 use App\Repository\ParametreRepository;
+use App\Repository\CompetenceRepository;
 
 
 final class FormationController extends AbstractController
