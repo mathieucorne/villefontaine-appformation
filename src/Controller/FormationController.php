6d7e4ba28bc24cc2359148e0d0f5d9fb1b31b480
Arthur Richert--Villain <saulevant@proton.me>
2025-07-03 13:46:50 +0200

    création de formation

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -4,10 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\FormationRepository;
 use App\Repository\ParametreRepository;
-use App\Repository\CompetenceRepository;
+use App\Entity\Formation;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 
 final class FormationController extends AbstractController
@@ -27,4 +30,29 @@ final class FormationController extends AbstractController
             'formations' => $formation
         ]);
     }
+    #[Route('/formation/creer', name: 'app_formation_creer', methods: ['POST'])]
+    public function creer(Request $request, EntityManagerInterface $em): RedirectResponse
+    {
+        $titre = $request->request->get('titre');
+        $description = $request->request->get('description');
+        $estVisible = $request->request->get('estVisible') ? true : false;
+
+        if (!$titre) {
+         $this->addFlash('error', 'Le titre est obligatoire.');
+            return $this->redirectToRoute('app_formation');
+        }
+
+        $formation = new Formation();
+        $formation->setTitre($titre);
+        $formation->setDescription($description);
+        $formation->setEstVisible($estVisible);
+
+        $em->persist($formation);
+        $em->flush();
+
+        $this->addFlash('success', 'Formation créée avec succès.');
+
+        return $this->redirectToRoute('app_formation');
+    }
+
 }
