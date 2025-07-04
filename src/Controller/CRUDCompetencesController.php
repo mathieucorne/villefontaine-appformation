667e842250b6f89b0c2cbedbb81bb88450acca58
Mathieu Corne <math.corne@gmail.com>
2025-07-04 15:46:38 +0200

    fix(crud_competence) - Correction du nom du controller

--- src/Controller/CRUDCompetencesController.php
+++ src/Controller/CRUDCompetencesController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class CRUDCompetenceController extends AbstractController
+final class CRUDCompetencesController extends AbstractController
 {
     #[Route('/competences', name: 'app_crud_competence')]
     public function index(): Response
