73ec41c340313a1353b048eb8b77280b4469f107
Mathieu Corne <math.corne@gmail.com>
2025-07-08 11:05:41 +0200

    refactor(formation_competence) - Ajout de groupes de sérialisation pour l'API

--- src/Entity/FormationCompetence.php
+++ src/Entity/FormationCompetence.php
@@ -5,20 +5,27 @@ namespace App\Entity;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\FormationCompetenceRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: FormationCompetenceRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    normalizationContext: ['groups' => ['formationcompetence:read']],
+    denormalizationContext: ['groups' => ['formationcompetence:write']]
+)]
 class FormationCompetence
 {
+    #[Groups(['formationcompetence:read'])]
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Groups(['formationcompetence:read', 'formationcompetence:write'])]
     #[ORM\ManyToOne(inversedBy: 'competences')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Formation $formation = null;
 
+    #[Groups(['formationcompetence:read', 'formationcompetence:write'])]
     #[ORM\ManyToOne(inversedBy: 'formations')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Competence $competence = null;
