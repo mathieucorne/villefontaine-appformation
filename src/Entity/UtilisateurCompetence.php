bef4dab51e28edd4e42e1fc20564add20ce12824
Mathieu Corne <math.corne@gmail.com>
2025-07-08 11:06:41 +0200

    refactor(utilisateur_competence) - Ajout de groupes de sérialisation pour l'API

--- src/Entity/UtilisateurCompetence.php
+++ src/Entity/UtilisateurCompetence.php
@@ -5,20 +5,27 @@ namespace App\Entity;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\UtilisateurCompetenceRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: UtilisateurCompetenceRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    normalizationContext: ['groups' => ['utilisateurcompetence:read']],
+    denormalizationContext: ['groups' => ['utilisateurcompetence:write']]
+)]
 class UtilisateurCompetence
 {
+    #[Groups(['utilisateurcompetence:read'])]
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Groups(['utilisateurcompetence:read', 'utilisateurcompetence:write'])]
     #[ORM\ManyToOne(inversedBy: 'competences')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Utilisateur $utilisateur = null;
 
+    #[Groups(['utilisateurcompetence:read', 'utilisateurcompetence:write'])]
     #[ORM\ManyToOne(inversedBy: 'utilisateurs')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Competence $competence = null;
