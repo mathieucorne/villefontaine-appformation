6628ca839d4ffa6f2a6584cae0b9a30a332686b1
Mathieu Corne <math.corne@gmail.com>
2025-07-16 11:05:03 +0200

    feat(formation) - Ajout des champs pour le CRUD Formation

--- src/Controller/Admin/FormationCrudController.php
+++ src/Controller/Admin/FormationCrudController.php
@@ -4,7 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Formation;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -15,14 +17,16 @@ class FormationCrudController extends AbstractCrudController
         return Formation::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
+            TextField::new('titre'),
             TextEditorField::new('description'),
+            ImageField::new('image_url')
+            ->setUploadDir('public\img\formations')
+            ->setBasePath('public\img\formations')
+            ->setRequired(false),
+            BooleanField::new('est_visible')
         ];
     }
-    */
 }
