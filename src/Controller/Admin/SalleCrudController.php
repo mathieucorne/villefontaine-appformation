4e8e511cdd63943cd6e04066f6daea7473366804
Mathieu Corne <math.corne@gmail.com>
2025-07-16 11:05:32 +0200

    feat(salle) - Ajout des champs pour le CRUD Salle

--- src/Controller/Admin/SalleCrudController.php
+++ src/Controller/Admin/SalleCrudController.php
@@ -7,6 +7,8 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 
 class SalleCrudController extends AbstractCrudController
 {
@@ -15,14 +17,14 @@ class SalleCrudController extends AbstractCrudController
         return Salle::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('nom'),
+            TextEditorField::new('batiment'),
+            IntegerField::new('nbPlacesMax'),
+            CollectionField::new('sessions')
         ];
     }
-    */
 }
