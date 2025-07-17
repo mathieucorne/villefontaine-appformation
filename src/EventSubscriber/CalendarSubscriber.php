3a596c0699a90baa408e15a63b70e64cbdd63a64
Mathieu Corne <math.corne@gmail.com>
2025-07-17 14:58:37 +0200

    feat(calendar, session) - Ajout d'un lien cliquable entre les événéments du Calendrier et la session associée grâce à un routage dynamique

--- src/EventSubscriber/CalendarSubscriber.php
+++ src/EventSubscriber/CalendarSubscriber.php
@@ -7,16 +7,22 @@ use CalendarBundle\Entity\Event;
 use CalendarBundle\Event\SetDataEvent;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Routing\RouterInterface;
 
 class CalendarSubscriber implements EventSubscriberInterface
 {
     private SessionRepository $sessionRepository;
     private Security $security;
+    private RouterInterface $router;
 
-    public function __construct(SessionRepository $sessionRepository, Security $security)
-    {
+    public function __construct(
+        SessionRepository $sessionRepository, 
+        Security $security,
+        RouterInterface $router
+    ) {
         $this->sessionRepository = $sessionRepository;
         $this->security = $security;
+        $this->router = $router;
     }
 
     public static function getSubscribedEvents()
@@ -36,10 +42,18 @@ class CalendarSubscriber implements EventSubscriberInterface
         $sessions = $this->sessionRepository->findMesSessions($utilisateur);
 
         foreach ($sessions as $session) {
+            $url = $this->router->generate('app_session', [
+                'id' => $session->getId()
+            ]);
             $setDataEvent->addEvent(new Event(
                 $session->getTitre(),
                 $session->getHeureDebut(),
-                $session->getHeureFin()
+                $session->getHeureFin(),
+                null,
+                [
+                    'url' => $url
+                ]
+
             ));
         }
     }
