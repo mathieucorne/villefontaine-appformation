c5926eacb7c7c5974de72e4ae6857bd4feec305c
Mathieu Corne <math.corne@gmail.com>
2025-07-18 11:34:35 +0200

    feat(participation) - ajout des méthodes findParticipation, inscrireUtilisateur, desinscrireUtilisateur et documentation de la méthode findParticipationsUtilisateur

--- src/Repository/ParticipationRepository.php
+++ src/Repository/ParticipationRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Participation;
+use App\Entity\Session;
 use App\Entity\Utilisateur;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -17,15 +18,68 @@ class ParticipationRepository extends ServiceEntityRepository
         parent::__construct($registry, Participation::class);
     }
 
+    /**
+     * @return Participation[] Renvoie un tableau d'objets Participations associé à un Utilisateur
+     */
     public function findParticipationsUtilisateur(Utilisateur $utilisateur): array {
         return $this->createQueryBuilder('p')
-            ->innerJoin('p.session', 's')
-            ->innerJoin('s.formation', 'f')
-            ->addSelect('s', 'f')
             ->andWhere('p.utilisateur = :utilisateur')
             ->setParameter('utilisateur', $utilisateur)
             ->getQuery()
             ->getResult();
+    }
+
+    /**
+     * @return Participation Renvoie un objet Participation associé à un Utilisateur et une Session
+     */
+    public function findParticipation(Utilisateur $utilisateur, Session $session) {
+        return $this->findOneBy([
+            'utilisateur' => $utilisateur,
+            'session' => $session
+        ]);
+    }
+
+    /**
+     * @return bool Inscrit un Utilisateur à une Session en créant une Participation
+     */
+    public function inscrireUtilisateur(
+        Utilisateur $utilisateur, 
+        Session $session
+    ): bool {
+        $participation = $this->findParticipation($utilisateur, $session);
+
+        if ($participation) {
+            return false;
+        } 
+
+        $newParticipation = new Participation();
+        $newParticipation->setUtilisateur($utilisateur);
+        $newParticipation->setSession($session);
+        $newParticipation->setDateInscription(new \DateTime());
+
+        
+        $this->getEntityManager()->persist($newParticipation);
+        $this->getEntityManager()->flush();
+
+        return true;
+    }
+
+    /**
+     * @return bool Désinscrit un Utilisateur à une Session en supprimant une Participation
+     */
+    public function desinscrireUtilisateur(
+        Utilisateur $utilisateur, 
+        Session $session
+    ): bool {
+        $participation = $this->findParticipation($utilisateur, $session);
+
+        if (!$participation) {
+            return false;
+        }
+
+        $this->getEntityManager()->remove($participation);
+        $this->getEntityManager()->flush();
+        return true;
     }
 
     //    /**
