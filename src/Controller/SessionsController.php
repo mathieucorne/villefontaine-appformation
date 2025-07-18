2686b049fb440a0d756cf94b6d6ba31ff25efd0f
Mathieu Corne <math.corne@gmail.com>
2025-07-18 11:36:22 +0200

    feat(session) - Ajout des routes inscription et désinscription à des sessions

--- src/Controller/SessionsController.php
+++ src/Controller/SessionsController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Utilisateur;
 use App\Entity\Session;
+use App\Repository\ParticipationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -11,11 +14,47 @@ final class SessionsController extends AbstractController
 {
     #[Route('/sessions/{id}', name: 'app_session')]
     public function index(
+        ParticipationRepository $participationRepository,
         Session $session
     ): Response
     {
+        /** @var Utilisateur $utilisateur */
+        $utilisateur = $this->getUser();
+
+        $participations = $participationRepository->findParticipationsUtilisateur($utilisateur);
+
+        $participationsSessionsId = array_map(
+            fn($p) => $p->getSession()->getId(),
+            $participations
+        );
+
         return $this->render('formations/session.html.twig', [
-            'session' => $session
+            'session' => $session,
+            'participationsSessionsId' => $participationsSessionsId
         ]);
+    }
+
+    #[Route('/sessions/{id}/inscription', name: 'app_session_inscription')]
+    public function inscrire(
+        Session $session,
+        Security $security,
+        ParticipationRepository $participationRepository
+    ) {
+        $utilisateur = $security->getUser();
+        $participationRepository->inscrireUtilisateur($utilisateur, $session);
+
+        return $this->redirectToRoute('app_home');
+    }
+
+    #[Route('/sessions/{id}/desinscription', name: 'app_session_desinscription')]
+    public function desinscrire(
+        Session $session,
+        Security $security,
+        ParticipationRepository $participationRepository
+    ) {
+        $utilisateur = $security->getUser();
+        $participationRepository->desinscrireUtilisateur($utilisateur, $session);
+
+        return $this->redirectToRoute('app_home');
     }
 }
