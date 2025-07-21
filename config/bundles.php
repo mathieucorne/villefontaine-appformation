7ada75bd6e02eddfcd22f5865c254846363cb934
Mathieu Corne <math.corne@gmail.com>
2025-07-21 10:21:41 +0200

    dep(chart-js) - Installation des dépendances pour ChartJS

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     CalendarBundle\CalendarBundle::class => ['all' => true],
+    Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
 ];
