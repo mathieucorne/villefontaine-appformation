0ef4de372b35bf1f93199999b9e26d28af1383ca
Mathieu Corne <math.corne@gmail.com>
2025-07-23 10:12:35 +0200

    feat(session) - Ajout de l'attribut dateLimiteInscription dans la page CRUD

--- src/Controller/Admin/SessionCrudController.php
+++ src/Controller/Admin/SessionCrudController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use App\EasyAdmin\Field\ServiceSelectorField;
 use App\Entity\Session;
+use App\Entity\SessionService;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
@@ -24,10 +27,44 @@ class SessionCrudController extends AbstractCrudController
             TextField::new('titre'),
             DateTimeField::new('heure_debut'),
             DateTimeField::new('heure_fin'),
+            DateTimeField::new('dateLimiteInscription'),
             AssociationField::new('formation'),
             AssociationField::new('salle'),
             IntegerField::new('nb_participants_max'),
-            AssociationField::new('services')
         ];
     }
+
+    // public function persistEntity(EntityManagerInterface $em, $entityInstance): void {
+    //     if(!$entityInstance instanceof Session) return;
+
+    //     $this->syncServices($entityInstance, $em);
+
+    //     parent::persistEntity($em, $entityInstance);
+    // }
+
+    // public function updateEntity(EntityManagerInterface $em, $entityInstance): void {
+    //     if($entityInstance instanceof Session) return;
+
+    //     $this->syncServices($entityInstance, $em);
+
+    //     parent::updateEntity($em, $entityInstance);
+
+    // }
+
+    // public function syncServices(Session $session, EntityManagerInterface $em): void {
+    //     foreach($session->getSessionServices() as $sessionService) {
+    //         $em->remove($sessionService);
+    //     }
+
+    //     $session->getSessionServices()->clear();
+
+    //     foreach($session->getServices() as $service) {
+    //         $sessionService = new SessionService();
+    //         $sessionService->setSession($session);
+    //         $sessionService->setService($service);
+
+    //         $em->persist($sessionService);
+    //         $session->getSessionServices()->add($sessionService);
+    //     }
+    // }
 }
